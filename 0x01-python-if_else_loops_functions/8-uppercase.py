554f563779bf42a954e4bc0ed0a71dcd45b6c140
FourtyThree43 <shaqmwa@outlook.com>
2023-03-07 21:24:39 +0300

    (ง'̀-'́)ง  ᕙ(⇀‸↼‶)ᕗ - Timestamp: 1678213479

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(s):
+    # To Convert ASCII to uppercase: (ord(c) - 32) i.e for a is (97-32) 65 = A.
     for c in s:
         uppercase_c = chr(ord(c) - 32) if ord(c) >= 97 and ord(c) <= 122 else c
         print('{}'.format(uppercase_c), end='')
