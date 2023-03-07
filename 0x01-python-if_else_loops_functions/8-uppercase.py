f9617507b57baa9c3dda8d16be531d6fc1c65bef
FourtyThree43 <shaqmwa@outlook.com>
2023-03-07 21:10:34 +0300

    (ಥ﹏ಥ)  ᕕ(⌐■_■)ᕗ ♪♬ - Timestamp: 1678212634

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Prints the given string in uppercase followed by a new line.
-
+Convert ASCII to uppercase is c -32 i.e (a - 32) = A.
 Args:
     str: A string to print in uppercase.
 """
@@ -10,7 +10,6 @@ Args:
 def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) < 123:
-            # Convert the character to uppercase using its ASCII code
             uppercase_c = chr(ord(str[i]) - 32)
         else:
             uppercase_c = str[i]
