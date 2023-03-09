7eb173fa3e088f1f15949e9b82bde1bb283b3c6a
FourtyThree43 <shaqmwa@outlook.com>
2023-03-09 21:26:54 +0300

    ٩(◕‿◕｡)۶  (╯°□°）╯︵ ┻━┻ - Timestamp: 1678386414

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
-if __name__ == "__main__":
-    from add_0 import add
+#!/usr/bin/python3
+from add_0 import add
 
 a = 1
 b = 2
 
-result_add = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result_add))
+result = add(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, result))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 if __name__ == "__main__":
-    # import the functions from calculator_1 .py
     from calculator_1 import add, sub, mul, div
 
-# define the variables a and b
 a = 10
 b = 5
 
