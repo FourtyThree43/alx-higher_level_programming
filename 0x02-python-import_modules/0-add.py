5133f6069964bf5ff30e7ac61e3a3a4d497fbf81
FourtyThree43 <shaqmwa@outlook.com>
2023-03-09 21:12:33 +0300

    ᕙ(⇀‸↼‶)ᕗ  ( ͡❛ ͜ʖ ͡❛) - Timestamp: 1678385553

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-
-# import the function add from add_0.py
-from add_0 import add
+if __name__ == "__main__":
+    # import the function add from add_0.py
+    from add_0 import add
 
 # define the variables a and b
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+
+result_add = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, result_add))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,13 @@ if __name__ == "__main__":
 # define the variables a and b
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+
+result_add = add(a ,b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, result_add))
+print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+print("{:d} / {:d} = {:d}".format(a, b, result_div))
