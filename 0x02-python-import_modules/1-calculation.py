9844792a92f18cb0f853379b357ae78a6e0395a7
FourtyThree43 <shaqmwa@outlook.com>
2023-03-09 21:28:17 +0300

    ᕙ(⇀‸↼‶)ᕗ  (╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯) - Timestamp: 1678386496

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
@@ -9,6 +8,7 @@ result_add = add(a, b)
 result_sub = sub(a, b)
 result_mul = mul(a, b)
 result_div = div(a, b)
+
 print("{:d} + {:d} = {:d}".format(a, b, result_add))
 print("{:d} - {:d} = {:d}".format(a, b, result_sub))
 print("{:d} * {:d} = {:d}".format(a, b, result_mul))
