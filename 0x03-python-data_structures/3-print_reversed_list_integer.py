5ee0e2c2faa54adc54d67e1256f55a9ed1aa14c9
FourtyThree43 <shaqmwa@outlook.com>
2023-03-13 16:39:54 +0300

    Update: (range(len(my_list))) - Timestamp: 1678714794

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print("{:d}".format(num))
+    for num in reversed(range(len(my_list))):
+        print("{:d}".format(my_list[num]))
