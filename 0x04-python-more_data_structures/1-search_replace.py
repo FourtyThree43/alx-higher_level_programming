7c81411d8b20610df0ec644761b4e77cd4bf2aee
FourtyThree43 <shaqmwa@outlook.com>
2023-03-17 22:09:25 +0300

    Fix Syntax - Timestamp: 1679080165

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,16 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element in a new list.
+
+    Args:
+        my_list (list): The original list.
+        search: The element to replace in the list.
+        replace: The new element.
+
+    Returns:
+        list: A new list with all occurrences of `search`
+        replaced by `replace`.
+    """
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
