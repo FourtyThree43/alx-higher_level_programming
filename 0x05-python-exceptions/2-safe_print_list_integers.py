e97d2aa756106701013aeeba50aa2862a0a3a059
FourtyThree43 <shaqmwa@outlook.com>
2023-03-28 15:47:52 +0300

    (╯°□°)╯︵ ʞooqǝɔɐɟ  (ʘ‿ʘ) - Timestamp: 1680007672

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,6 @@ def safe_print_list_integers(my_list=[], x=0):
     element_count = 0
     for element in range(x):
         try:
-    #    if type(my_list[element]) == int:
             print("{:d}".format(my_list[element]), end="")
             element_count += 1
         except (TypeError, ValueError):
