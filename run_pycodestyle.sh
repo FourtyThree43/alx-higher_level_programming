80d3ace9927a75e6abbadfd26b2cfdc46f7d050a
FourtyThree43 <shaqmwa@outlook.com>
2023-03-29 16:13:08 +0300

    Task Init..

--- run_pycodestyle.sh
+++ run_pycodestyle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -name '*.py' ! -name '*main*' ! -name '*test*' -exec pycodestyle {} +
+find . -name '*.py' ! -name '*main*' ! -name '*test*' -exec pycodestyle --show-source {} +
 
 
 printf "\nCheck Done!\n"
