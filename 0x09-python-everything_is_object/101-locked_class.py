abeec9763efff7ceddeb9544a38693487b6f138d
FourtyThree43 <shaqmwa@outlook.com>
2023-04-04 23:24:59 +0300

    ᕕ(⌐■_■)ᕗ ♪♬  ┌( ಠ‿ಠ)┘ - Timestamp: 1680639899

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """Defines a locked class."""
 
+
 class LockedClass:
     """
-    A class that prevents the user from dynamically creating new instance attributes,
-    except for an attribute called 'first_name'.
+    A class that prevents the user from dynamically creating
+    new instance attributes, except for an attribute called 'first_name'.
     """
     __slots__ = ['first_name']
 
     def __init__(self):
         """Initialize the instance with a 'first_name' attribute."""
         self.first_name = None
-    
+
     def __setattr__(self, key, value):
         """
-        Called when an instance attribute is set. Raises an AttributeError if
-        the attribute being set is not 'first_name' and it does not already exist.
+        Called when an instance attribute is set.
+        Raises an AttributeError if the attribute being set is not
+        'first_name' and it does not already exist.
         """
         if key != 'first_name' and not hasattr(self, 'first_name'):
-            raise AttributeError(f"'LockedClass' object has no attribute '{key}'")
+            raise AttributeError(f"'LockedClass' object has no attribute\
+                                 '{key}'")
         super().__setattr__(key, value)
