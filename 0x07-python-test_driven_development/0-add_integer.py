626653a3e0de52c63ee8d1c1c74c6d3e04e45180
FourtyThree43 <shaqmwa@outlook.com>
2023-04-05 00:07:33 +0300

    Task 1 - Timestamp: 1680642453

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    add two integersa + b
+
+    Args:
+        a (int, float): the first integer to add
+        b (int, float): the second integer to add,. Defaults to 98.
+
+    Raises:
+        TypeError:  if a or b are not integers or floats
+
+    Returns:
+        int: the addition of a and b
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
