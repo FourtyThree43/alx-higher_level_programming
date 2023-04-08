5e1a6a4df0988bc04270f8b11eb1ae1aa3168aed
FourtyThree43 <shaqmwa@outlook.com>
2023-04-08 22:26:11 +0300

    (ง ื▿ ื)ว  (╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯) - Timestamp: 1680981971

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,7 +18,7 @@ def print_square(size):
         None
     """
 
-    if not isinstance(size, (int, float)):
+    if not isinstance(size, (int)):
         raise TypeError("size must be an integer")
 
     if size < 0:
@@ -27,8 +27,7 @@ def print_square(size):
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
-    if isinstance(size, float):
-        size = int(size)
-
     for i in range(size):
-        print("#" * size)
+        for j in range(size):
+            print("#", end='')
+        print()
