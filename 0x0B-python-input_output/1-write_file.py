f569c00c8bc9d1ec9a40f2db02db8f421f2246df
FourtyThree43 <shaqmwa@outlook.com>
2023-04-13 22:44:21 +0300

    Task 0-2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
+'''Module to writes a string to a text file'''
+
+
+def write_file(filename="", text=""):
+    """
+    fuction that writes a string to text file (UTF8)
+    Args:
+        filename - name of the file to write to.
+        text(str) - the string to write
+    Return:
+        Number of charater written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
