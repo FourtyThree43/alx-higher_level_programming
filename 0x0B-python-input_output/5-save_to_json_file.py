9f6a48952962af47f62441af58a6b14e1b083d4a
FourtyThree43 <shaqmwa@outlook.com>
2023-04-14 00:47:25 +0300

    Revert back

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,7 +9,4 @@ def save_to_json_file(my_obj, filename):
     using a JSON representation
     """
     with open(filename, "w") as f:
-        try:
-            json.dump(my_obj, f)
-        except TypeError as e:
-            print(f"Error: {e}")
+        json.dump(my_obj, f)
