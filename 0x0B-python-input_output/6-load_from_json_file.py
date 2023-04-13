f1705bf31333614e83d51194523666d2eda90518
FourtyThree43 <shaqmwa@outlook.com>
2023-04-14 00:32:06 +0300

    Task 6 - 9

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
+"""Module load form json file"""
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    function that creates an Object from a “JSON file”
+    '''
+    with open(filename) as f:
+        json.load(f)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
+"""Module class Student."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age) -> None:
+        """
+        Initialize a new Student object
+
+        Args:
+            first_name (str): The student first's name.
+            last_name (str): The student last's name.
+            age (int): The student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self) -> dict:
+        """Retrieves a dictionary representation of the Student."""
+        return self.__dict__
