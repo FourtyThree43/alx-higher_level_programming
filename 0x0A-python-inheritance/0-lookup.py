1a99b1c072fb00f3db12e7c5d40481c581b87edb
FourtyThree43 <shaqmwa@outlook.com>
2023-04-13 15:00:07 +0300

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+'''Module lookup the list of available attributes and methods of an object:'''
+
+
+def lookup(obj):
+    '''Returns the list of available attributes and methods of an object'''
+    return sorted(dir(obj))
