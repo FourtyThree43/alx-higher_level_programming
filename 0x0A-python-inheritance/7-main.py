e4f689f72d796f29ef4de91840ab1db96105b2eb
FourtyThree43 <shaqmwa@outlook.com>
2023-04-13 21:59:06 +0300

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  (╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯) - Timestamp: 1681412346

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -20,3 +20,43 @@ try:
     bg.integer_validator("distance", -4)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", None)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", {3, 4})
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", [3])
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", (4))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", "4")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
