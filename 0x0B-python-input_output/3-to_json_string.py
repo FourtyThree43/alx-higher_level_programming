8c914e7fe26f24d17fb11eac74931ac72100681a
FourtyThree43 <shaqmwa@outlook.com>
2023-04-13 23:30:25 +0300

    Task 3-5

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
+"""Module to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation
+    of an object (string)
+    """
+    return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
+"""Module form_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string
+    """
+    return json.loads(my_str)
