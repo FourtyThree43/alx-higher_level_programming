f9273eb7d7a2482750682d6baf7debd2557581c2
FourtyThree43 <shaqmwa@outlook.com>
2023-04-14 14:05:46 +0300

    Sytax update

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,15 +10,15 @@ def pascal_triangle_1(n):
     if n <= 0:
         return []
 
-    pas_tri = [[1]]
-    while len(pas_tri) != n:
-        prev_row = pas_tri[-1]
-        new_row = [1]
+    triangle = [[1]]
+    while len(triangle) != n:
+        prev_row = triangle[-1]
+        curr_row = [1]
         for i in range(len(prev_row)-1):
-            new_row += [prev_row[i] + prev_row[i + 1]]
-        new_row += [1]
-        pas_tri.append(new_row)
-    return pas_tri
+            curr_row += [prev_row[i] + prev_row[i + 1]]
+        curr_row += [1]
+        triangle.append(curr_row)
+    return triangle
 
 
 def factorial(n):
