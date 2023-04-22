139acd006c839ed8d088f6c57ad8a50082f02a4f
FourtyThree43 <shaqmwa@outlook.com>
2023-04-22 03:42:42 +0300

    Tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 '''Module for unittests for Rectangle class'''
 
+from re import S
 import unittest
+import json
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
     '''Tests for Base class'''
@@ -39,6 +42,11 @@ class TestBase(unittest.TestCase):
         r2 = Rectangle.create(**r_dict)
         self.assertEqual(str(r), str(r2))
 
+        sq1 = Square(1, 2, 3, 4)
+        sq_dict = sq1.to_dictionary()
+        sq2 = Square.create(**sq_dict)
+        self.assertEqual(str(sq1), str(sq2))
+
     def test_to_json_string(self):
         # Test passing empty list to method
         empty_list = []
@@ -50,12 +58,29 @@ class TestBase(unittest.TestCase):
         r2 = Rectangle(1, 2)
         r_list = [r1, r2]
         r_json = Base.to_json_string([r.to_dictionary() for r in r_list])
-        self.assertEqual(r_json, '[{"id": 8, "width": 4, "height": 5, "x": 6, "y": 0}, {"id": 9, "width": 1, "height": 2, "x": 0, "y": 0}]')
+        r_list_deserialized = json.loads(r_json)    # Deserialize the JSON string back into a list of dictionaries,
+        r_json_reserialized = json.dumps(r_list_deserialized) # then reserialize the list and compare to the original JSON string.
+        self.assertEqual(r_json, r_json_reserialized)
+
+        sq1 = Square(4, 5, 6)
+        sq2 = Square(1, 2)
+        sq_list = [sq1, sq2]
+        sq_json = Base.to_json_string([sq1.to_dictionary() for sq1 in sq_list])
+        sq_list_deserialized = json.loads(sq_json)    # Deserialize the JSON string back into a list of dictionaries,
+        sq_json_reserialized = json.dumps(sq_list_deserialized) # then reserialize the list and compare to the original JSON string.
+        self.assertEqual(sq_json, sq_json_reserialized)
 
         r = Rectangle(1, 2, 3, 4, 5)
         r_dict = r.to_dictionary()
         r_json = Base.to_json_string([r_dict])
         self.assertEqual(r_json, '[{"id": 5, "width": 1, "height": 2, "x": 3, "y": 4}]')
+
+        sq1 = Square(1, 2, 3, 4)
+        sq_dict = sq1.to_dictionary()
+        sq_json = Base.to_json_string([sq_dict])
+        sq_list_deserialized = json.loads(sq_json)    # Deserialize the JSON string back into a list of dictionaries,
+        sq_json_reserialized = json.dumps(sq_list_deserialized) # then reserialize the list and compare to the original JSON string.
+        self.assertEqual(sq_json, sq_json_reserialized)
 
     def test_from_json_string(self):
         # Test converting empty string to list
@@ -63,10 +88,11 @@ class TestBase(unittest.TestCase):
         self.assertEqual(empty_list, [])
 
         # Test loading list of Rectangle objects from JSON string
-        r_dict_list = [{"id": 8, "width": 4, "height": 5, "x": 6, "y": 0}, {"id": 9, "width": 1, "height": 2, "x": 0, "y": 0}]
-        r_list = [Rectangle.create(**r_dict) for r_dict in r_dict_list]
-        r_json = Base.to_json_string([r.to_dictionary() for r in r_list])
-        loaded_list = Base.from_json_string
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        r2 = Rectangle(2, 4, 5, 6, 2)
+        Rectangle.save_to_file([r1, r2])
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(str(r1), str(list_rectangles_output[0]))
 
         r_json = '[{"id": 5, "width": 1, "height": 2, "x": 3, "y": 4}]'
         obj_list = Base.from_json_string(r_json)
@@ -79,9 +105,126 @@ class TestBase(unittest.TestCase):
         with open("Rectangle.json", "r") as file:
             self.assertEqual(file.read(), '[{"id": 5, "width": 1, "height": 2, "x": 3, "y": 4}, {"id": 1, "width": 5, "height": 4, "x": 3, "y": 2}]')
 
+        s = Square(1, 2, 3, 4)
+        s2 = Square(5, 4, 3, 2)
+        Square.save_to_file([s, s2])
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(), '[{"id": 4, "size": 1, "x": 2, "y": 3}, {"id": 2, "size": 5, "x": 4, "y": 3}]')
+
     def test_load_from_file(self):
         r = Rectangle(1, 2, 3, 4, 5)
         r2 = Rectangle(5, 4, 3, 2, 1)
         Rectangle.save_to_file([r, r2])
         obj_list = Rectangle.load_from_file()
         self.assertEqual(len(obj_list), 2)
+
+        s = Square(1, 2, 3, 4)
+        s2 = Square(5, 4, 3, 2)
+        Square.save_to_file([s, s2])
+        obj_list = Square.load_from_file()
+        self.assertEqual(len(obj_list), 2)
+
+class TestFromJsonString(unittest.TestCase):
+
+    def test_list_output_type(self):
+        list_input = [{"id": 89, "width": 10, "height": 4}]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertIsInstance(list_output, list)
+
+    def test_one_rectangle_output(self):
+        list_input = [{"id": 89, "width": 10, "height": 4, "x": 7}]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_input, list_output)
+
+    def test_two_rectangles_output(self):
+        list_input = [
+            {"id": 89, "width": 10, "height": 4, "x": 7, "y": 8},
+            {"id": 98, "width": 5, "height": 2, "x": 1, "y": 3},
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_input, list_output)
+
+    def test_one_square_output(self):
+        list_input = [{"id": 89, "size": 10, "height": 4}]
+        json_list_input = Square.to_json_string(list_input)
+        list_output = Square.from_json_string(json_list_input)
+        self.assertEqual(list_input, list_output)
+
+    def test_two_squares_output(self):
+        list_input = [
+            {"id": 89, "size": 10, "height": 4},
+            {"id": 7, "size": 1, "height": 7}
+        ]
+        json_list_input = Square.to_json_string(list_input)
+        list_output = Square.from_json_string(json_list_input)
+        self.assertEqual(list_input, list_output)
+
+    def test_None_input(self):
+        self.assertEqual([], Base.from_json_string(None))
+
+    def test_empty_list_input(self):
+        self.assertEqual([], Base.from_json_string("[]"))
+
+    def test_no_args_input(self):
+        with self.assertRaises(TypeError):
+            Base.from_json_string()
+
+    def test_more_than_one_arg_input(self):
+        with self.assertRaises(TypeError):
+            Base.from_json_string([], 1)
+
+import unittest
+
+class TestBase_create(unittest.TestCase):
+    """Unittests for testing create method of Base class."""
+
+    def test_create_rectangle_original(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
+
+    def test_create_rectangle_new(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r2))
+
+    def test_create_rectangle_is(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertIsNot(r1, r2)
+
+    def test_create_rectangle_equals(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertNotEqual(r1, r2)
+
+    def test_create_square_original(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertEqual("[Square] (7) 5/1 - 3", str(s1))
+
+    def test_create_square_new(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertEqual("[Square] (7) 5/1 - 3", str(s2))
+
+    def test_create_square_is(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertIsNot(s1, s2)
+
+    def test_create_square_equals(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertNotEqual(s1, s2)
