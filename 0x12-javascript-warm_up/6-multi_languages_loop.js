f265e47adab038816e57c3a627d7a7c3c85e3e71
FourtyThree43 <shaqmwa@outlook.com>
2023-06-12 17:42:42 +0300

    Loop to languages Update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,4 @@
 // script that prints odjets in 3 lines.
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-lines.forEach(function (line) {
-  console.log(line);
-});
+lines.forEach((line) => { console.log(line); });
