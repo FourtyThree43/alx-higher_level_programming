9ed09c6f0a26bd4033a7fe165012235a5fb49e07
FourtyThree43 <shaqmwa@outlook.com>
2023-06-12 21:26:49 +0300

    add(a, b)

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints the addition of 2 integers
+
+function add (a, b) {
+  return (a + b);
+}
+
+const args = process.argv.slice(2);
+const arg1 = parseInt(args[0]);
+const arg2 = parseInt(args[1]);
+
+console.log(add(arg1, arg2));
