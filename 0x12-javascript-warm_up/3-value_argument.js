ae581faab4f02b10621b7b90c27da226a988a167
FourtyThree43 <shaqmwa@outlook.com>
2023-06-12 17:05:58 +0300

    fix to check onlyy first arg

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints the first argument passed to it.
 const args = process.argv.slice(2);
-const hasArgs = Boolean(args[0]);
+const firstArg = args[0];
 
-console.log(hasArgs ? args.join('\n') : 'No argument');
+console.log(firstArg || 'No argument');
