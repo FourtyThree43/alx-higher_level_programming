0019825247cff7a18e17d55f3cba741a3a1110ca
FourtyThree43 <shaqmwa@outlook.com>
2023-06-12 21:03:22 +0300

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints x times “C is fun”
+const args = process.argv.slice(2);
+const x = parseInt(args[0]); // number of occurrences
+
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
