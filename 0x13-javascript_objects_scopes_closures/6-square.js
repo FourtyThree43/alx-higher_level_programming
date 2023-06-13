eba2eb2c176506319813aa82be12990a567563cf
FourtyThree43 <shaqmwa@outlook.com>
2023-06-13 19:05:56 +0300

    Square Class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// class Square that defines a square and inherits from Rectangle
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
