af7b0fdad7771b02c8cb064c0dac880c989f43fb
FourtyThree43 <shaqmwa@outlook.com>
2023-06-29 22:47:48 +0300

    checker test awk

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | perl -ne 'print $1 if /^Allow:\s*(.*)/i'
+curl -sI "$1" | awk -F': ' '/^Allow:/ { print $2 }'
