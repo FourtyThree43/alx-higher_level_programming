4d2f5c7edcaab817e0e062758a5cb9cd22a2fe50
FourtyThree43 <shaqmwa@outlook.com>
2023-06-29 23:26:51 +0300

    (ง^ᗜ^)ง  (╬ಠ益ಠ) - Timestamp: 1688070411

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that get the message You got me!
-curl -s -X POST -d "user_id=42" "http://0.0.0.0:5000/catch_me" | grep -o "You got me!"
+curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
