24c8e61298d308916d2bef94b8b6fde581dacb47
FourtyThree43 <shaqmwa@outlook.com>
2023-07-04 19:11:55 +0300

    fetches status + header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-'''DOC'''
+"""
+This module sends a request to a specified URL and
+displays the value of the X-Request-Id header.
+"""
+from urllib.request import urlopen, Request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = Request(url)
+
+    with urlopen(req) as response:
+        header = response.getheader("X-Request-Id")
+
+    print(header)
