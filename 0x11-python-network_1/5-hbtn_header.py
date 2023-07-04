dde08031986286797d5f1a47f616039bafadb0e2
FourtyThree43 <shaqmwa@outlook.com>
2023-07-04 21:41:58 +0300

    Task 02 - 07

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-'''DOC'''
+"""
+This module sends a request to a specified URL and
+displays the value of the X-Request-Id header.
+using the requests package.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url: str = argv[1]
+
+    req = requests.get(url)
+    header = req.headers.get("X-Request-Id")
+
+    print(header)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-'''DOC'''
+"""
+This module sends a request to a specified URL
+and displays the body of the response.
+using the requests package.
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url: str = argv[1]
+    req = requests.get(url)
+    status = req.status_codes
+    body = req.text
+
+    if status >= 400:
+        print(f"Error code: {status}")
+    else:
+        print(body)
