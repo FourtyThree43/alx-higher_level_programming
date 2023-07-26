a9c4d2c1c3db99c4dba02cefd70352c8f6e9c114
FourtyThree43 <shaqmwa@outlook.com>
2023-07-26 16:01:49 +0300

    For real, this time.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,16 +11,11 @@ request.get(apiURL, (err, response, body) => {
     const completed = {};
     const taskData = JSON.parse(body);
 
-    for (const i in taskData) {
-      const task = taskData[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+    taskData.forEach((task) => {
+      if (task.completed) {
+        completed[task.userId] = (completed[task.userId] || 0) + 1;
       }
-    }
+    });
     console.log(completed);
   } else {
     console.log(`Error code: ${response.statusCode}`);
