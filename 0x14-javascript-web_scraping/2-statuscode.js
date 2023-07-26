ac4719660d0e9a7c29b89f8a12d3e9161d5879b5
FourtyThree43 <shaqmwa@outlook.com>
2023-07-26 12:59:54 +0300

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 
 const myURL = process.argv[2]
 request.get(myURL).on('response', (response) => {
-  console.log(response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 });
